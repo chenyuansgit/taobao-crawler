a1f57e885993f3ed33fed05ecc9bbd1caa3e0d76
Han Jiang <han@apache.org>
2015-09-12 16:30:26 +0800

    use jquery for utils

--- autopaging.js
+++ autopaging.js
@@ -1,25 +1,68 @@
-var byClass;
-byClass = document.getElementsByClassName("page-cur");
-console.assert(byClass.length == 1, "page no. missing?");
-var pageNum = byClass[0].innerHTML;
-alert(pageNum);
-var ack = pageNum == '2' ? 'end' : 'ongoing';
-
-// gen message
-var message = {type : "info", page : pageNum, ack : ack};
-
-chrome.runtime.sendMessage(message, function(response) {
-  if (response && response.ack) {
-    if (response.ack == "got") {
-      var byClass = document.getElementsByClassName("page-next");
-      console.assert(byClass.length == 1, "button missing?");
-      var nextPage = byClass[0];
-      nextPage.click()
-      console.log("go next page");
-    } else if (response.ack == 'done') {
-      console.log("crawling done");
+function DeepTrim(obj) {
+  for (var prop in obj) {
+    var value = obj[prop], type = typeof value;
+    if (value != null && (type == "string" || type == "object") && obj.hasOwnProperty(prop)) {
+      if (type == "object") {
+        DeepTrim(obj[prop]);
+      } else {
+        obj[prop] = obj[prop].trim();
+      }
     }
-  } else {
-    console.log("info response missing");
   }
-});
+}
+
+function GoNextPage() {
+  $(".page-next")[0].click();
+}
+
+
+function main() {
+
+  var targetRegex = /qudao.gongxiao.tmall.com/;
+  if (!targetRegex.test(document.location.href)) {
+    console.log("redirecting...");
+    return;
+  }
+  
+  console.log("recording...");
+
+  var items = $("#J_InviteList").find('tbody').find('.item');
+  var data = [];
+  for (var i = 0, max = items.length; i < max; i++) {
+    var tds = $(items[i]).children();
+    data.push({
+      user_id     : $(tds[0]).children(':first').html(),
+      user_link   : $(tds[0]).children(':first').attr('href'),
+      user_icon   : $(tds[0]).children(':first').next().html(),
+      level       : $(tds[1]).html(),
+      rate        : $(tds[2]).html(),
+      open_date   : $(tds[3]).html(),
+      type        : $(tds[4]).html(),
+      invite_date : $(tds[5]).html(),
+      invite_mode : $(tds[6]).html(),
+      invite_stat : $(tds[7]).children(':first').html()
+    });
+  }
+  DeepTrim(data);
+  alert(JSON.stringify(data));
+
+  // gen message
+  var pageNum = $(".page-cur").html();
+  var ack = pageNum >= '2' ? 'end' : 'ongoing';  // nocommit
+  var message = {type : "info", page : pageNum, ack : ack};
+
+  chrome.runtime.sendMessage(message, function(response) {
+    if (response && response.ack) {
+      if (response.ack == "got") {
+        GoNextPage();
+        console.log("go next page");
+      } else if (response.ack == 'done') {
+        console.log("crawling done");
+      }
+    } else {
+      console.log("info response missing");
+    }
+  });
+}
+
+main();
