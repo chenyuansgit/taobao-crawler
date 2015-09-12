72a4559345450efe31af918cb8c69c9efce8491a
Han Jiang <han@apache.org>
2015-09-12 12:06:29 +0800

    add paging util

--- autopaging.js
+++ autopaging.js
@@ -1,4 +1,25 @@
-var buttons = document.getElementsByClassName("page-next");
-console.assert(buttons.length == 1, "button missing?")
-alert("done")
-buttons[0].click()
+var byClass;
+byClass = document.getElementsByClassName("page-cur");
+console.assert(byClass.length == 1, "page no. missing?");
+var pageNum = byClass[0].innerHTML;
+alert(pageNum);
+var ack = pageNum == '2' ? 'end' : 'ongoing';
+
+// gen message
+var message = {type : "info", page : pageNum, ack : ack};
+
+chrome.runtime.sendMessage(message, function(response) {
+  if (response && response.ack) {
+    if (response.ack == "got") {
+      var byClass = document.getElementsByClassName("page-next");
+      console.assert(byClass.length == 1, "button missing?");
+      var nextPage = byClass[0];
+      nextPage.click()
+      console.log("go next page");
+    } else if (response.ack == 'done') {
+      console.log("crawling done");
+    }
+  } else {
+    console.log("info response missing");
+  }
+});
