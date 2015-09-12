37d65fddef906bbe4c8ad9be14074a9ad55ae6ab
Han Jiang <han@apache.org>
2015-09-12 17:08:49 +0800

    add csv utils

--- autopaging.js
+++ autopaging.js
@@ -1,3 +1,7 @@
+function RemoveLineBreak(str) {
+  return str.replace(/(\r\n|\n|\r)/gm," ");
+}
+
 function DeepTrim(obj) {
   for (var prop in obj) {
     var value = obj[prop], type = typeof value;
@@ -6,6 +10,7 @@ function DeepTrim(obj) {
         DeepTrim(obj[prop]);
       } else {
         obj[prop] = obj[prop].trim();
+        obj[prop] = RemoveLineBreak(obj[prop]);
       }
     }
   }
@@ -13,6 +18,10 @@ function DeepTrim(obj) {
 
 function GoNextPage() {
   $(".page-next")[0].click();
+}
+
+function CheckLastPage() {
+  // do sth to see whether we're at last page
 }
 
 
@@ -27,14 +36,14 @@ function main() {
   console.log("recording...");
 
   var items = $("#J_InviteList").find('tbody').find('.item');
-  var data = [];
+  var json = [];
   for (var i = 0, max = items.length; i < max; i++) {
     var tds = $(items[i]).children();
-    data.push({
+    json.push({
       user_id     : $(tds[0]).children(':first').html(),
-      user_link   : $(tds[0]).children(':first').attr('href'),
-      user_icon   : $(tds[0]).children(':first').next().html(),
-      level       : $(tds[1]).html(),
+//      user_link   : 'http:' + $(tds[0]).children(':first').attr('href'),
+//      user_icon   : $(tds[0]).children(':first').next().html(),
+//      level       : $(tds[1]).html(),
       rate        : $(tds[2]).html(),
       open_date   : $(tds[3]).html(),
       type        : $(tds[4]).html(),
@@ -43,13 +52,29 @@ function main() {
       invite_stat : $(tds[7]).children(':first').html()
     });
   }
-  DeepTrim(data);
-  alert(JSON.stringify(data));
+  DeepTrim(json);
+
+  var fields = Object.keys(json[0]);
+  var csv = json.map(function(row){
+    return fields.map(function(fieldName){
+      return '"' + (row[fieldName] || '') + '"';
+    });
+  });
+  csv.unshift(fields); // add header column
+  csv = csv.join('\r\n');
+
+  alert(csv);
+  console.log(csv);
+  //window.open( "data:application/csv;charset=utf-8," + escape(csv));
 
   // gen message
   var pageNum = $(".page-cur").html();
-  var ack = pageNum >= '2' ? 'end' : 'ongoing';  // nocommit
-  var message = {type : "info", page : pageNum, ack : ack};
+  var ack = pageNum >= '1' ? 'end' : 'ongoing';  // nocommit
+  var message = {
+    type : "info", 
+    data : json,
+    page : pageNum,
+    ack  : ack};
 
   chrome.runtime.sendMessage(message, function(response) {
     if (response && response.ack) {
