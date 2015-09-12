b7f9f2ca227accf1ea124cc161286a9be968fc9e
Han Jiang <han@apache.org>
2015-09-12 11:22:45 +0800

    minor change

--- autofill.js
+++ autofill.js
@@ -3,5 +3,7 @@ var val="123456789a"
 
 document.getElementById("TPL_username_1").value = key;
 document.getElementById("TPL_password_1").value = val;
+//hmm...infinite loop?
+//document.getElementById("J_SubmitStatic").click()
 
 
